1bba2b1402cb4af7c2f0a15b009d05f790dd0b21
QuinnPooch <pucciquinn@gmail.com>
2022-02-03 11:42:49 -0700

    added dashboard and routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
